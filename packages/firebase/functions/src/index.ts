3f6545ae94f76a577e33257b357b02211c9788df
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-10-08 23:29:46 +0900

    feat(firebase): subscribe error log on stackDriver

--- packages/firebase/functions/src/index.ts
+++ packages/firebase/functions/src/index.ts
@@ -1,19 +1,24 @@
 import * as functions from "firebase-functions";
-import { initializeApp, firestore, credential } from "firebase-admin";
+import { initializeApp, firestore } from "firebase-admin";
 
 import onCreateUser from "./trigger/onCreateUser";
 import onDeleteUser from "./trigger/onDeleteUser";
 import onCreatePlaylog from "./trigger/onCreatePlaylog";
 import onWriteHighscores from "./trigger/onWriteHighscores";
 
+import cloudFunctionsWarnLog from "./pubsub/cloudFunctionsWarnLog";
+
 initializeApp();
 firestore().settings({
   timestampsInSnapshots: true
 });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((_request, response) => {
   response.send("Hello from Firebase!");
 });
 
 // Trigger
 export { onCreateUser, onDeleteUser, onCreatePlaylog, onWriteHighscores };
+
+// pubsub
+export { cloudFunctionsWarnLog };
