d305ad1253aa99a836c87bb9f0b67e2e959dfc25
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-10-28 13:48:24 +0900

    feat(ranking): show game list with model

--- packages/ranking/app/ts/components/pages/Index.tsx
+++ packages/ranking/app/ts/components/pages/Index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import AutoBind from "autobind-decorator";
+import { GAMES, Game, gameIds } from "@sokontokoro/mikan";
 
 import { IndexRouteParams } from "../../App";
 import HeaderSection from "../organisms/HeaderSection";
@@ -11,35 +12,8 @@ import FooterSection from "../organisms/FooterSection";
 interface Props {}
 
 interface State {
-  initialIndex: number;
-  game: string;
+  game: Game;
 }
-
-const domain =
-  process.env.NODE_ENV === "production"
-    ? "games.sokontokoro-factory.net"
-    : "games-dev.sokontokoro-factory.net";
-
-export const list = [
-  {
-    title: "ほのCar",
-    gameId: "honocar",
-    url: `https://${domain}/honocar/`,
-    imageUrl: `http://${domain}/honocar/img/TITLE_LOGO_HONOKA.png`
-  },
-  {
-    title: "しゃかりん",
-    gameId: "shakarin",
-    url: `https://${domain}/shakarin/`,
-    imageUrl: `http://${domain}/shakarin/img/TITLE_LOGO.png`
-  },
-  {
-    title: "まるてん",
-    gameId: "maruten",
-    url: `https://${domain}/maruten/`,
-    imageUrl: `http://${domain}/maruten/img/TITLE_LOGO_HANAMARU.png`
-  }
-];
 
 @AutoBind
 class Index extends React.Component<
@@ -51,24 +25,21 @@ class Index extends React.Component<
   constructor(props: any) {
     super(props);
 
-    const initialIndex = list.findIndex(({ gameId }) => {
-      return gameId === this.props.match.params.game;
-    });
+    const { game } = this.props.match.params;
 
     this.state = {
-      initialIndex,
-      game: list[initialIndex].gameId
+      game
     };
   }
 
   public render() {
-    const { initialIndex, game } = this.state;
+    const { game } = this.state;
 
     return (
       <React.Fragment>
         <HeaderSection />
         <ControlSection
-          initialIndex={initialIndex}
+          game={game}
           onGameSelected={this.onGameSelected}
           onJumpGame={this.onJumpGame}
         />
@@ -79,19 +50,16 @@ class Index extends React.Component<
   }
 
   private onGameSelected(index: number) {
-    this.props.history.replace(`/ranking/${list[index].gameId}`);
+    this.props.history.replace(`/ranking/${gameIds[index]}`);
 
     this.setState({
-      game: list[index].gameId
+      game: gameIds[index]
     });
   }
 
   private onJumpGame() {
-    const index = list.findIndex(({ gameId }) => {
-      return gameId === this.props.match.params.game;
-    });
-
-    location.href = list[index].url;
+    const { game } = this.props.match.params;
+    location.href = GAMES[game].url;
   }
 }
 
