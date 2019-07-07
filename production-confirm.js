93b54340ecff04cae3f1c53001b4c13fda3d394c
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-07-07 11:34:05 +0900

    ci(*): define production workflow to production-deploy

--- production-confirm.js
+++ production-confirm.js
@@ -4,8 +4,14 @@ const stdin = process.openStdin();
 const CONFIRM_MESSAGE = `🔥PRODUCTION SCRIPT🔥\nDo you want to continue this script? [y/N]: `;
 const OK_MESSAGE = `✨ OK! Continue scripts!\n`;
 const CANCEL_MESSAGE = `👿 CANCEL....\n`;
+const IGNORE_MESSAGE = `🍊 I'm circleci and ignore this confirmation!\n`;
 
 process.stdout.write(CONFIRM_MESSAGE);
+
+if (require("os").userInfo().username === "circleci") {
+  process.stdout.write(IGNORE_MESSAGE);
+  process.exit(0);
+}
 
 stdin.on("data", function(chunk) {
   if (chunk.indexOf("y") === 0) {
