c9a4fb09615f1e00594929e7a01af608ab4b5749
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2019-07-13 21:25:25 +0900

    feat(firebase): support upload twitter mediaData

---
+++ packages/firebase/src/https/uploadImageToTwitter.ts
@@ -0,0 +1,40 @@
+import { https, config } from "firebase-functions";
+import * as Twitter from "twitter";
+
+const client = new Twitter({
+  consumer_key: config().twitter.consumer_key,
+  consumer_secret: config().twitter.consumer_secret,
+  access_token_key: config().twitter.access_token_key,
+  access_token_secret: config().twitter.access_token_secret
+});
+
+export default https.onCall(async (data, _context) => {
+  const { mediaData } = data;
+
+  if (!mediaData) {
+    return {
+      error: true,
+      message: "mediaData is required."
+    };
+  }
+  try {
+    const { media_id_string } = await client.post("media/upload", {
+      media_data: mediaData
+    });
+    const status = await client.post("statuses/update.json", {
+      status: `__${new Date().toDateString()}__`,
+      media_ids: media_id_string
+    });
+
+    return {
+      url: status.entities.media[0].url
+    };
+  } catch (e) {
+    console.error("error", e);
+
+    return {
+      error: true,
+      message: e.message
+    };
+  }
+});
