4a834d4bf30ecb04c23c53fa6653da49fe4683a2
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-11-13 15:44:32 +0900

    show tweet button

--- packages/rinadoko/app/src/state/GameResultState.ts
+++ packages/rinadoko/app/src/state/GameResultState.ts
@@ -1,10 +1,13 @@
 import * as PIXI from "pixi.js";
 import { State, StateEnterParams, stateMachineService } from "../index";
 import { Result } from "../model/Result";
+import { tweetByWebIntent } from "@sokontokoro/mikan/dist/utils";
 
 export class GameResultState implements State {
   public static nodeKey = "@game-result";
 
+  private shareController: HTMLElement;
+  private twitterShare: HTMLElement;
   private pixiState: HTMLElement;
   private result: Result;
 
@@ -12,6 +15,10 @@ export class GameResultState implements State {
 
   onEnter({ context }: StateEnterParams) {
     const { resources } = this.context.app.loader;
+
+    this.shareController = document.getElementById("share-controller");
+    this.twitterShare = document.getElementById("twitter-share");
+
     this.result = new Result({
       scale: this.context.scale,
       screen: {
@@ -34,13 +41,42 @@ export class GameResultState implements State {
 
     this.pixiState = document.getElementById("pixi");
     this.pixiState.addEventListener("pointerdown", this.onTapStage);
+    this.twitterShare.addEventListener("pointerdown", this.onClickTwitterShare);
+
+    this.showShareController();
   }
   onExit({ context }) {
     this.context.app.stage.removeChild(this.result.container);
     this.pixiState.removeEventListener("pointerdown", this.onTapStage);
+    this.twitterShare.removeEventListener(
+      "pointerdown",
+      this.onClickTwitterShare
+    );
   }
 
   onTapStage() {
     stateMachineService.send("RESTART");
   }
+
+  showShareController() {
+    this.shareController.classList.remove("share-controller--hide");
+  }
+
+  onClickTwitterShare = () => {
+    let text = `全然見つけられなかった...`;
+    if (0 < this.result.point) {
+      text = `${this.result.point}回見つけられたよ！`;
+    }
+
+    tweetByWebIntent({
+      text,
+      url: "https://games.sokontokoro-factory.net/rinadoko/",
+      hashtags: [
+        "リナちゃんボックスどこ",
+        "そこんところ工房",
+        "lovelive",
+        "虹ヶ咲"
+      ]
+    });
+  };
 }
