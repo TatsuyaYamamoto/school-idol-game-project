c6f9741edac27a1675cc055b7e234a233303c344
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-07-13 18:15:52 +0900

    build(portal): remove ranking package

--- packages/ranking/app/ts/components/atoms/GameImage.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import styled from "styled-components";
-
-const Image = styled.img`
-  height: 300px;
-  margin: 0 auto;
-`;
-
-export default Image;
--- packages/ranking/app/ts/components/atoms/GameTitle.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import styled from "styled-components";
-
-const Title = styled.div`
-  font-size: 30px;
-`;
-
-export default Title;
